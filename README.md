a67a415f0138da8414c63d3ef401f2f96e3a7ee8
yoonchaiyoung <domo62@naver.com>
2020-08-20 17:41:24 +0900

    java code

---
+++ README.md
@@ -0,0 +1,8 @@
+# helloworld
+
+### 다양한 helloworld 예제
+
+#### 1. C 버젼
+#### 2. python 버젼
+#### 3. shell script 버젼
+
---
+++ java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+	public static void main(String[] args){
+		String s = "Hello World";
+		System.out.println(s);
+	}
+
+}
